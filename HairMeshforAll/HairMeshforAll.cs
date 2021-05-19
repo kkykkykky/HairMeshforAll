4029610e0057053b12d07a08c34e9aff3aa6a50d
KKY <8879012+kkykkykky@users.noreply.github.com>
2021-05-18 23:53:34 -0600

    Initial commit

---
+++ HairMeshforAll/HairMeshforAll.cs
@@ -0,0 +1,46 @@
+﻿using AIChara;
+using BepInEx;
+using HarmonyLib;
+
+namespace HairMeshforAll
+{
+    [BepInPlugin(GUID, "Hair Mesh for All", Version)]
+    public partial class HairMeshforAll : BaseUnityPlugin
+    {
+        public const string GUID = "kky.HairMeshforAll";
+        public const string Version = "1.0.0";
+
+        private static CmpHair hairComponent;
+        private static bool goAhead;
+
+        public static void Logging(BepInEx.Logging.LogLevel level, string _text)
+        {
+            BepInEx.Logging.Logger.CreateLogSource(nameof(HairMeshforAll)).Log(level, _text);
+        }
+
+        private void Main()
+        {
+            Harmony.CreateAndPatchAll(typeof(HairMeshforAll));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(AIChara.ChaControl), "ChangeSettingHairMeshType")]
+
+        public static void EnableUseMesh_Patch(AIChara.ChaControl __instance, int parts)
+        {
+            goAhead = false;
+            hairComponent = __instance.GetCustomHairComponent(parts);
+            if (hairComponent != null)
+            {
+                goAhead = hairComponent.rendHair != null && hairComponent.rendHair.Length > 0 && !hairComponent.useMesh;
+                if (goAhead) DoEnableUseMesh(hairComponent);
+            }
+        }
+
+        private static void DoEnableUseMesh(CmpHair customHairComponent)
+        {
+            customHairComponent.useMesh = true;
+            Logging(BepInEx.Logging.LogLevel.Debug, $"{customHairComponent.name} mesh enabled");
+        }
+    }
+}
---
+++ HairMeshforAll/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(nameof(HairMeshforAll))]
+[assembly: AssemblyDescription("Hair Mesh for All")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("kkykkykky")]
+[assembly: AssemblyProduct(nameof(HairMeshforAll))]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b717eab0-3c3b-4c12-a810-610c70d4c337")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(HairMeshforAll.HairMeshforAll.Version)]
+[assembly: AssemblyFileVersion(HairMeshforAll.HairMeshforAll.Version)]
